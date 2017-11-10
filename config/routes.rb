c968515a1f1520fe386f72220681820c28fe854f
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-11-10 15:52:13 -0700

    added stripe to app

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   resources :locations
   resources :categories
 
+  resources :charges
+
   root "pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
