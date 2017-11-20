38737716d3bbc3b9035aa3895ec5108244c7e35a
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-11-20 14:32:34 -0700

    got single map working for locations

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,7 @@ Rails.application.config.assets.precompile += %w( locations_theme.css )
 Rails.application.config.assets.precompile += %w( locations_theme.js )
 Rails.application.config.assets.precompile += %w( locations_maps.css )
 Rails.application.config.assets.precompile += %w( locations_maps.js )
+Rails.application.config.assets.precompile += %w( locations_locations.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
