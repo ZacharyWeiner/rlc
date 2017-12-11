3e7ba6ba3bd7b2ffafaa1183d65d82a6bb977163
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-12-11 09:48:36 -0700

    added suppport for redirect based on shuttle subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :direct_to_app
   protect_from_forgery with: :exception
+
+  def direct_to_app
+    if  request.subdomains.include?('shuttle')
+      if request.original_url == "resortlodgingcompany.com"
+        redirect_to new_ride_request_path
+      end
+    end
+  end
 end
