be6d2fecef48f36b6ab6a0024e4585474a763d67
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-12-11 09:53:08 -0700

    updated logging for live version on url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def direct_to_app
+    p request.domain
     if  request.subdomains.include?('shuttle')
+      p request.subdomains
       if request.original_url == "resortlodgingcompany.com"
+        p request.original_url
         redirect_to new_ride_request_path
       end
     end
