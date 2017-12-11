57d60830b76b67f3a0c2d848ff29a6a909fe29c9
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-12-11 10:02:58 -0700

    more logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
     p "Domain " + request.domain
     if  request.subdomains.include?('shuttle')
       p "Subdomains: " + request.subdomains.first
+      p 'Original Url' + request.original_url
       if request.original_url == "resortlodgingcompany.com"
-        p 'Original Url' + request.original_url
         redirect_to new_ride_request_path
       end
     end
