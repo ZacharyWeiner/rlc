bd4a99fb512b74e859760b1e9e7140e30e7a7464
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-12-21 12:49:41 -0700

    fixed issue with phone numbers

--- app/models/sms_manager.rb
+++ app/models/sms_manager.rb
@@ -3,13 +3,16 @@ class SmsManager
   attr_accessor :message, :to_number, :message
 
   def send_message
-    sid = ENV['TWILIO_SID']
-    token = ENV['TWILIO_AUTH_TOKEN']
-    @client = Twilio::REST::Client.new sid, token
-    @client.api.account.messages.create(
-      from: '+19708756350 ',
-      to: self.to_number,
-      body: self.message
-    )
+    if self.to_number.nil? || self.to_number.length < 9 || self.to_number == ""
+      return
+    end
+      sid = ENV['TWILIO_SID']
+      token = ENV['TWILIO_AUTH_TOKEN']
+      @client = Twilio::REST::Client.new sid, token
+      @client.api.account.messages.create(
+        from: '+19708756350 ',
+        to: self.to_number,
+        body: self.message
+      )
   end
 end
